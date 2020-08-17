3614696b3b9ace1f3ec03e76203ada077eec1d3e
grun00 <lucas.c.grunheidt@gmail.com>
2020-08-16 22:15:39 -0300

    Can increase break/session timers

--- src/App.js
+++ src/App.js
@@ -13,18 +13,45 @@ class App extends Component{
       minute: 25,
       hour: 0,
     }
+
+    this.onIncreaseBreakLength = this.onIncreaseBreakLength.bind(this);
+    this.onIncreaseSessionLength = this.onIncreaseSessionLength.bind(this);
   }
+  onIncreaseBreakLength(){
+    this.setState(prevState => {
+      return {
+        breakLength: prevState.breakLength + 1
+      };
+    })
+  }
+
+  onIncreaseSessionLength(){
+    this.setState(prevState => {
+      return {
+        sessionLength: prevState.sessionLength + 1
+      };
+    })
+  }
+
   render(){
     return (
       <div className="App" id="main">
         <h2> React Pomodoro </h2>
         <section class='interval-container'>
-          <SessionInterval interval={this.state.breakLength} title='Break Time'/>
+          <SessionInterval
+            interval={this.state.breakLength} title='Break Time'
+            increase={this.onIncreaseBreakLength}
+          />
           <div class='separator'></div>
-          <SessionInterval interval={this.state.sessionLength} title='Session Time'/>
+          <SessionInterval
+            interval={this.state.sessionLength} title='Session Time'
+            increase={this.onIncreaseSessionLength}
+          />
         </section>
         <div class='separator'></div>
-        <Timer timerMinute={this.state.minute} timerHour={this.state.hour}/>
+        <Timer
+          timerMinute={this.state.minute} timerHour={this.state.hour}
+        />
       </div>
     );
   }
--- src/components/Interval.js
+++ src/components/Interval.js
@@ -2,13 +2,27 @@ import React from 'react';
 import { IoIosArrowUp, IoIosArrowDown } from "react-icons/io";
 
 function SessionInterval(props) {
+  function minusCounter(){
+    if (props.interval === 1){
+      return;
+    }
+  }
+
+  function plusCounter(){
+    if (props.interval === 60){
+      return;
+    }
+
+    props.increase();
+  }
+
   return(
     <section class='interval-outer'>
         <h4>{props.title}</h4>
       <section class='interval-inner'>
-        <button> <IoIosArrowUp/> </button>
+        <button onClick={plusCounter}> <IoIosArrowUp/> </button>
         <p class='interval-length'> {props.interval}</p>
-        <button> <IoIosArrowDown/>  </button>
+        <button onClick={minusCounter}> <IoIosArrowDown/>  </button>
       </section>
     </section>
   )
